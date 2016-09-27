45ce45c5d4e7165bd416a6ddec88d12977d96577
Neli Hateva <nelly.hateva@gmail.com>
2016-09-27 17:56:53 +0300

    add USHMDate Similarity test

---
+++ src/test/java/com/ontotext/ehri/deduplication/tests/USHMMDateTest.java
@@ -0,0 +1,24 @@
+package com.ontotext.ehri.deduplication.tests;
+
+import com.ontotext.ehri.deduplication.measures.USHMMDate;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+public class USHMMDateTest {
+
+    private final double DELTA = 0.001;
+
+    @Test
+    public void testUSHMMDateSimilarity() throws Exception {
+
+        assertEquals(USHMMDate.similarity("19120931", "19120931"), 1, DELTA);
+        assertTrue(USHMMDate.similarity("19120931", "19120931") > USHMMDate.similarity("19120931", "19120901"));
+        assertTrue(USHMMDate.similarity("19120931", "19120901") > USHMMDate.similarity("19120931", "19120201"));
+        assertTrue(USHMMDate.similarity("19120931", "19120201") > USHMMDate.similarity("600", "18500512"));
+        assertTrue(USHMMDate.similarity("600", "18500512") > USHMMDate.similarity("19120713", "19130612"));
+
+    }
+
+}
