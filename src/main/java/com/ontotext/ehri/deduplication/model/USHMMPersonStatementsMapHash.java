6376f1d6db0c9bbffb53ce4f6bef615e5ae55010
Nelly Hateva <nelly.hateva@gmail.com>
2016-10-26 17:20:37 +0300

    move SPARQLEndpointConfig class in a separate package

--- src/main/java/com/ontotext/ehri/deduplication/model/USHMMPersonStatementsMapHash.java
+++ src/main/java/com/ontotext/ehri/deduplication/model/USHMMPersonStatementsMapHash.java
@@ -1,5 +1,6 @@
 package com.ontotext.ehri.deduplication.model;
 
+import com.ontotext.ehri.deduplication.sparql.SPARQLEndpointConfig;
 import com.ontotext.ehri.deduplication.utils.SerializationUtils;
 import org.openrdf.query.*;
 import org.openrdf.repository.RepositoryConnection;
@@ -7,9 +8,6 @@ import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.http.HTTPRepository;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 
 /**
@@ -169,42 +167,6 @@ class USHMMPersonStatementsMapHash {
                 "    ?s ushmm:personId \"" + personId + "\".\n" +
                 "    " + predicate + "\n" +
                 "}");
-    }
-
-    private static class SPARQLEndpointConfig {
-
-        String repositoryURL;
-        String username;
-        String password;
-
-        SPARQLEndpointConfig() {
-            InputStream inputStream = null;
-            try {
-                Properties prop = new Properties();
-                String propFileName = "ehri_sparql_endpoint.properties";
-
-                inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-
-                if (inputStream != null) {
-                    prop.load(inputStream);
-                } else {
-                    throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-                }
-
-                repositoryURL = prop.getProperty("ehri.sparql.endpoint.repository.url");
-                username = prop.getProperty("ehri.sparql.endpoint.username");
-                password = prop.getProperty("ehri.sparql.endpoint.password");
-
-            } catch (Exception e) {
-                System.out.println("Exception: " + e);
-            } finally {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
     private static class USHMMQueryResultHandler implements TupleQueryResultHandler {
